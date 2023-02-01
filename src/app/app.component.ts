8ab6a33441104073709079c639fa722103c16cf7
jossalajop <102058287+jossalajop@users.noreply.github.com>
2023-01-31 21:06:07 -0500

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client/JoselynAlajo/3BV/ProgramacionVisual';
+  title = 'client/JoselynAlajoPunina/3BV/ProgramacionVisual';
 }
